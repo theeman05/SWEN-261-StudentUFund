ce191e9b730f66f5622bb28abe9a86745fe11e3c
Ethan Hartman <ehh4525@rit.edu>
2023-10-05 20:57:51 -0400

    Created Supporter Class

---
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/model/Supporter.java
@@ -0,0 +1,44 @@
+package com.ufund.api.ufundapi.model;
+
+import java.util.ArrayList;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * Represents a Supporter entity
+ * 
+ * @author Ethan Hartman
+ */
+public class Supporter extends User{
+    static final String STRING_FORMAT = "Supporter [name=%s,type=%s,fundingBasket=%s]";
+
+    @JsonProperty("funding_basket") private ArrayList<Need> fundingBasket;
+
+    /**
+     * Create a user with the given username and type
+     * Their funding basket will be empty
+     * @param username The username of the user
+     * @param fundingBasket The list of {@linkplain Need needs} in this user's funding basket
+     */
+    public Supporter(@JsonProperty("username") String username, @JsonProperty("funding_basket") ArrayList<Need> fundingBasket) {
+        super(username);
+        this.fundingBasket = fundingBasket;
+    }
+
+    /**
+     * Get the list of {@linkplain Need needs} in this user's funding basket
+     * Note: This should probably only be used on initializding a supporter
+     * @return The arraylist of {@linkplain Need needs} in this user's funding basket
+     */
+    public ArrayList<Need> getFundingBasket() {
+        return fundingBasket;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String toString() {
+        return String.format(STRING_FORMAT, username, isAdmin(), fundingBasket);
+    }
+}
