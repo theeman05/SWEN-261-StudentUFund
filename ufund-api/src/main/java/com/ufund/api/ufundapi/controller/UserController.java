a084ae02e144f6cb58137f7a06e9cab8273ceb3d
Ethan Hartman <ehh4525@rit.edu>
2023-10-06 19:19:21 -0400

    Began Starting UserController and changed LoginController name to UserController.

---
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/controller/UserController.java
@@ -0,0 +1,103 @@
+package com.ufund.api.ufundapi.controller;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ufund.api.ufundapi.exceptions.SupporterNotSignedInException;
+
+import com.ufund.api.ufundapi.model.Need;
+import com.ufund.api.ufundapi.model.User;
+import com.ufund.api.ufundapi.persistence.UserDAO;
+
+/**
+ * Handles the REST API requests for the User resource
+ * 
+ * @author Ethan Hartman
+ */
+
+@RestController
+@RequestMapping("users")
+public class UserController {
+    private static final Logger LOG = Logger.getLogger(UserController.class.getName());
+    private final UserDAO userDAO;
+
+    /**
+     * Creates a REST API controller to reponds to requests
+     * 
+     * @param userDAO The {@link UserDAO User Data Access Object} to perform CRUD operations
+     * <br>
+     * This dependency is injected by the Spring Framework
+     */
+    public UserController(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
+
+    /**
+     * Responds to the GET request for a {@linkplain User user} with the given username
+     * Will log in the user with the given username, if a user was found
+     * 
+     * @param username The username used to locate the {@link User user}
+     * 
+     * @return ResponseEntity with {@link User user} object and HTTP status of OK if found<br>
+     * ResponseEntity with HTTP status of NOT_FOUND if not found<br>
+     * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     */
+    @GetMapping("/{username}")
+    public ResponseEntity<User> loginUser(@PathVariable String username) {
+        LOG.info("GET /users/" + username);
+        try {
+            User user = userDAO.getUser(username);
+            if (user != null){
+                userDAO.loginUser(user);
+                return new ResponseEntity<User>(user, HttpStatus.OK);
+            }else
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }catch(IOException e) {
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * Responds to the GET request for logging out
+     * Will log out the current user, if there is one
+     * 
+     * @return ResponseEntity with a boolean value of True and HTTP status of OK
+     */
+    @GetMapping("/logout")
+    public ResponseEntity<Boolean> logout() {
+        LOG.info("GET /logout");
+        userDAO.logoutCurUser();
+        return new ResponseEntity<>(true, HttpStatus.OK);
+    }
+
+    /**
+     * Responds to the GET request for retreiving the current user's basket
+     * 
+     * @return ResponseEntity with a boolean value of True and HTTP status of OK if supporter is signed in<br>
+     * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     */
+    @GetMapping("/basket")
+    public ResponseEntity<Need[]> getCurBasket() {
+        LOG.info("GET /basket");
+        try {
+            return new ResponseEntity<Need[]>(userDAO.getCurBasket(), HttpStatus.OK);
+        }catch(SupporterNotSignedInException e) {
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
