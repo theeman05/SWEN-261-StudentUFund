d3ce210c24380ceebd57233aafaf58b216d968f8
Ethan Hartman <ehh4525@rit.edu>
2023-10-05 20:57:41 -0400

    Created User Class

--- ufund-api/src/main/java/com/ufund/api/ufundapi/model/User.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/model/User.java
@@ -1,5 +1,49 @@
 package com.ufund.api.ufundapi.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * Represents a User entity
+ * 
+ * @author Ethan Hartman
+ */
 public class User {
-    
+    static final String STRING_FORMAT = "User [name=%s, isAdmin=%s]";
+    static final String ADMIN_USERNAME = "admin";
+
+    @JsonProperty("username") protected String username;
+
+    /**
+     * Create a user with the given username and type
+     * Their funding basket will be empty
+     * @param username The username of the user
+     * @param type The type of the user
+     */
+    public User(@JsonProperty("username") String username) {
+        this.username = username;
+    }
+
+    /**
+     * Get the username of the user
+     * @return The username of the user
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * Check if the user is an admin
+     * @return True if the user is an admin, false otherwise
+     */
+    public boolean isAdmin() {
+        return username.equals(ADMIN_USERNAME);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String toString() {
+        return String.format(STRING_FORMAT, username, isAdmin());
+    }
 }
