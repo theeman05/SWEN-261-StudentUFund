fa51f8b92c9c2d40f31128a9d27f1e65f1242b19
happihound <bevanneib@gmail.com>
2023-10-11 17:45:25 -0400

    reformatted

--- ufund-api/src/main/java/com/ufund/api/ufundapi/controller/NeedController.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/controller/NeedController.java
@@ -21,7 +21,6 @@ import java.util.logging.Logger;
 
 import javax.management.openmbean.KeyAlreadyExistsException;
 
-
 /**
  * Handles the REST API requests for the Need resource
  * 
@@ -37,9 +36,10 @@ public class NeedController {
     /**
      * Creates a REST API controller to reponds to requests
      * 
-     * @param needDao The {@link NeedDAO Need Data Access Object} to perform CRUD operations
-     * <br>
-     * This dependency is injected by the Spring Framework
+     * @param needDao The {@link NeedDAO Need Data Access Object} to perform CRUD
+     *                operations
+     *                <br>
+     *                This dependency is injected by the Spring Framework
      */
     public NeedController(NeedDAO needDao) {
         this.needDao = needDao;
@@ -50,9 +50,10 @@ public class NeedController {
      * 
      * @param name The name used to locate the {@link Need need}
      * 
-     * @return ResponseEntity with {@link Need need} object and HTTP status of OK if found<br>
-     * ResponseEntity with HTTP status of NOT_FOUND if not found<br>
-     * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     * @return ResponseEntity with {@link Need need} object and HTTP status of OK if
+     *         found<br>
+     *         ResponseEntity with HTTP status of NOT_FOUND if not found<br>
+     *         ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
      */
     @GetMapping("/{name}")
     public ResponseEntity<Need> getNeed(@PathVariable String name) {
@@ -60,11 +61,11 @@ public class NeedController {
         try {
             Need need = needDao.getNeed(name);
             if (need != null)
-                return new ResponseEntity<Need>(need,HttpStatus.OK);
+                return new ResponseEntity<Need>(need, HttpStatus.OK);
             else
                 return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }catch(IOException e) {
-            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+        } catch (IOException e) {
+            LOG.log(Level.SEVERE, e.getLocalizedMessage());
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
@@ -72,41 +73,45 @@ public class NeedController {
     /**
      * Responds to the GET request for all {@linkplain Need needs}
      * 
-     * @return ResponseEntity with array of {@link Need need} objects (may be empty) and
-     * HTTP status of OK<br>
-     * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     * @return ResponseEntity with array of {@link Need need} objects (may be empty)
+     *         and
+     *         HTTP status of OK<br>
+     *         ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
      */
     @GetMapping("")
     public ResponseEntity<Need[]> getNeeds() {
         LOG.info("GET /needs");
-        try{
-             return new ResponseEntity<Need[]>(needDao.getNeeds(), HttpStatus.OK);
-        }catch (IOException e){
-            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+        try {
+            return new ResponseEntity<Need[]>(needDao.getNeeds(), HttpStatus.OK);
+        } catch (IOException e) {
+            LOG.log(Level.SEVERE, e.getLocalizedMessage());
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     /**
-     * Responds to the GET request for all {@linkplain Need needs} whose name contains
+     * Responds to the GET request for all {@linkplain Need needs} whose name
+     * contains
      * the text in name
      * 
-     * @param name The name parameter which contains the text used to find the {@link Need needs}
+     * @param name The name parameter which contains the text used to find the
+     *             {@link Need needs}
      * 
-     * @return ResponseEntity with array of {@link Need need} objects (may be empty) and
-     * HTTP status of OK<br>
-     * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
-     * <p>
-     * Example: Find all needs whose name contain the text "ma"
-     * GET http://localhost:8080/needs/?name=ma
+     * @return ResponseEntity with array of {@link Need need} objects (may be empty)
+     *         and
+     *         HTTP status of OK<br>
+     *         ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     *         <p>
+     *         Example: Find all needs whose name contain the text "ma"
+     *         GET http://localhost:8080/needs/?name=ma
      */
     @GetMapping("/")
     public ResponseEntity<Need[]> searchNeeds(@RequestParam String name) {
-        LOG.info("GET /needs/?name="+name);
-        try{
+        LOG.info("GET /needs/?name=" + name);
+        try {
             return new ResponseEntity<Need[]>(needDao.findNeeds(name), HttpStatus.OK);
-        }catch (IOException e){
-            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+        } catch (IOException e) {
+            LOG.log(Level.SEVERE, e.getLocalizedMessage());
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
@@ -116,48 +121,52 @@ public class NeedController {
      * 
      * @param need - The {@link Need need} to create
      * 
-     * @return ResponseEntity with created {@link Need need} object and HTTP status of CREATED<br>
-     * ResponseEntity with HTTP status of CONFLICT if {@link Need need} object already exists<br>
-     * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     * @return ResponseEntity with created {@link Need need} object and HTTP status
+     *         of CREATED<br>
+     *         ResponseEntity with HTTP status of CONFLICT if {@link Need need}
+     *         object already exists<br>
+     *         ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
      */
     @PostMapping("")
     public ResponseEntity<Need> createNeed(@RequestBody Need need) {
         LOG.info("POST /needs " + need);
-        try{
+        try {
             Need createdNeed = needDao.createNeed(need);
             if (createdNeed != null)
                 return new ResponseEntity<Need>(createdNeed, HttpStatus.CREATED);
             else
                 return new ResponseEntity<>(HttpStatus.CONFLICT);
-        }catch (KeyAlreadyExistsException e){
-            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+        } catch (KeyAlreadyExistsException e) {
+            LOG.log(Level.SEVERE, e.getLocalizedMessage());
             return new ResponseEntity<>(HttpStatus.CONFLICT);
-        }catch (IOException e){
-            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+        } catch (IOException e) {
+            LOG.log(Level.SEVERE, e.getLocalizedMessage());
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     /**
-     * Updates the {@linkplain Need need} with the provided {@linkplain Need need} object, if it exists
+     * Updates the {@linkplain Need need} with the provided {@linkplain Need need}
+     * object, if it exists
      * 
      * @param need The {@link Need need} to update
      * 
-     * @return ResponseEntity with updated {@link Need need} object and HTTP status of OK if updated<br>
-     * ResponseEntity with HTTP status of NOT_FOUND if not found<br>
-     * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     * @return ResponseEntity with updated {@link Need need} object and HTTP status
+     *         of OK if updated<br>
+     *         ResponseEntity with HTTP status of NOT_FOUND if not found<br>
+     *         ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
      */
     @PutMapping("")
     public ResponseEntity<Need> updateNeed(@RequestBody Need need) {
         LOG.info("PUT /needs " + need);
-        try{
+        try {
             Need updatedNeed = needDao.updateNeed(need);
             if (updatedNeed != null)
                 return new ResponseEntity<Need>(updatedNeed, HttpStatus.OK);
             else
                 return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }catch (IOException e){
-            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+        } catch (IOException e) {
+            LOG.log(Level.SEVERE, e.getLocalizedMessage());
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
@@ -168,22 +177,21 @@ public class NeedController {
      * @param name The name of the {@link Need need} to deleted
      * 
      * @return ResponseEntity HTTP status of OK if deleted<br>
-     * ResponseEntity with HTTP status of NOT_FOUND if not found<br>
-     * ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
+     *         ResponseEntity with HTTP status of NOT_FOUND if not found<br>
+     *         ResponseEntity with HTTP status of INTERNAL_SERVER_ERROR otherwise
      */
     @DeleteMapping("/{name}")
     public ResponseEntity<Need> deleteNeed(@PathVariable String name) {
         LOG.info("DELETE /needs/" + name);
-        try{
+        try {
             boolean wasDeleted = needDao.deleteNeed(name);
             if (wasDeleted)
                 return new ResponseEntity<>(HttpStatus.OK);
             else
                 return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }catch (IOException e){
-            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+        } catch (IOException e) {
+            LOG.log(Level.SEVERE, e.getLocalizedMessage());
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
-
--- ufund-api/src/main/java/com/ufund/api/ufundapi/model/Need.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/model/Need.java
@@ -25,16 +25,22 @@ public class Need {
     // Package private for tests
     static final String STRING_FORMAT = "Need [name=%s,cost=$%s,quantity=%s,type=%s]";
 
-    @JsonProperty("name") private String name;
-    @JsonProperty("cost") private double cost;
-    @JsonProperty("quantity") private int quantity;
-    @JsonProperty("type") private NeedType type;
+    @JsonProperty("name")
+    private String name;
+    @JsonProperty("cost")
+    private double cost;
+    @JsonProperty("quantity")
+    private int quantity;
+    @JsonProperty("type")
+    private NeedType type;
 
     /**
      * Create a need with the given name, cost, quantity, and type
+     * 
      * @param name The name of the need
      */
-    public Need(@JsonProperty("name") String name, @JsonProperty("cost") double cost, @JsonProperty("quantity") int quantity, @JsonProperty("type") NeedType type) {
+    public Need(@JsonProperty("name") String name, @JsonProperty("cost") double cost,
+            @JsonProperty("quantity") int quantity, @JsonProperty("type") NeedType type) {
         this.name = name;
         this.cost = cost;
         this.quantity = quantity;
@@ -43,48 +49,70 @@ public class Need {
 
     /**
      * Sets the cost of the need
+     * 
      * @param cost The cost of the need
      */
 
-    public void setCost(double cost) {this.cost = cost;}
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
 
     /**
      * Sets the quantity of the need
+     * 
      * @param quantity The quantity of the need
      */
-    public void setQuantity(int quantity) {this.quantity = quantity;}
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 
     /**
      * Sets the type of the need
+     * 
      * @param type The type of the need
      */
 
-    public void setType(NeedType type) {this.type = type;}
+    public void setType(NeedType type) {
+        this.type = type;
+    }
 
     /**
      * Retrieves the name of the need
+     * 
      * @return The name of the need
      */
-    public String getName() {return name;}
+    public String getName() {
+        return name;
+    }
 
     /**
      * Retrieves the cost of the need
+     * 
      * @return The cost of the need
      */
 
-    public double getCost() {return cost;}
+    public double getCost() {
+        return cost;
+    }
+
     /**
      * Retrieves the quantity of the need
+     * 
      * @return The quantity of the need
      */
-    
-    public int getQuantity() {return quantity;}
+
+    public int getQuantity() {
+        return quantity;
+    }
 
     /**
      * Retrieves the type of the need
+     * 
      * @return The type of the need
      */
-    public NeedType getType() {return type;}
+    public NeedType getType() {
+        return type;
+    }
 
     /**
      * {@inheritDoc}
