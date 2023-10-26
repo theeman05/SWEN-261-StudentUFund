4fe15fc9bc93f2edfdef106e936a1264b455d6bf
Ethan Hartman <ehh4525@rit.edu>
2023-10-26 14:16:06 -0400

    Began the cupboard UI

--- ufund-ui/src/app/needs/needs.component.ts
+++ ufund-ui/src/app/needs/needs.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
-import { NEEDS } from '../mock-needs';
+import { Component, OnInit } from '@angular/core';
+import { Need } from '../need';
+import { NeedService } from '../need.service';
 
 @Component({
   selector: 'app-needs',
   templateUrl: './needs.component.html',
   styleUrls: ['./needs.component.css']
 })
-export class NeedsComponent {
-  needs = NEEDS;
+export class NeedsComponent implements OnInit {
+  needs: Need[] = [];
+  
+  constructor(private needService: NeedService) { }
+
+  ngOnInit(): void {
+    this.getNeeds();
+  }
+
+  getNeeds(): void {
+    this.needService.getNeeds().subscribe(needs => this.needs = needs);
+  }
+
+  delete(hero: Need): void {
+    this.needs = this.needs.filter(h => h !== hero);
+    //this.needService.deleteNeed(hero.name).subscribe();
+  }
 }
