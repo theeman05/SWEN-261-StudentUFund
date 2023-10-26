b163eb004b2c3143b3ab1076ce9a37385e6d9028
Ethan Hartman <ehh4525@rit.edu>
2023-10-26 14:33:42 -0400

    Updated routing module, components

--- ufund-ui/src/app/need.service.ts
+++ ufund-ui/src/app/need.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+import { Need } from './need';
+import { NEEDS } from './mock-needs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NeedService {
-
   constructor() { }
+
+  getNeeds(): Observable<Need[]> {
+    const needs = of(NEEDS);
+    return needs;
+  }
+
+  // /** DELETE: delete the need from the server */
+  // deleteNeed(id: number): Observable<Need> {
+  //   const url = `${this.needsURL}/${id}`;
+
+  //   return this.http.delete<Need>(url, this.httpOptions).pipe(
+  //     tap(_ => this.log(`deleted hero id=${id}`)),
+  //     catchError(this.handleError<Hero>('deleteHero'))
+  //   );
+  // }
 }
