1d7f14d566aa8d0dee65678cb882bb55d0d9cc6d
Ethan Hartman <ehh4525@rit.edu>
2023-10-26 16:20:22 -0400

    Accepted app-routing from this branch

--- ufund-ui/src/app/app-routing.module.ts
+++ ufund-ui/src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-<<<<<<< HEAD
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { NeedDetailComponent } from './need-detail/need-detail.component';
 import { NeedsComponent } from './needs/needs.component';
@@ -18,12 +17,3 @@ const routes: Routes = [
   exports: [ RouterModule ]
 })
 export class AppRoutingModule {}
-=======
-const routes: Routes = [];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
->>>>>>> 6c636b4d508941431378f1df66a6b7e09928916b
