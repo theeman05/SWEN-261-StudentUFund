3d69ff540def26a51bbd5a2e807e612cec21e176
Ben Griffin <bgg6007@rit.edu>
2023-10-29 16:14:16 -0400

    add to basket updated html and OnInit ts

--- ufund-ui/src/app/basket/basket.component.ts
+++ ufund-ui/src/app/basket/basket.component.ts
@@ -1,21 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { NeedService } from '../need.service';
+import { UserService } from '../user.service';
+import { Need } from '../need';
 
 @Component({
   selector: 'app-basket',
   templateUrl: './basket.component.html',
   styleUrls: ['./basket.component.css']
 })
-export class BasketComponent{
-
-  constructor(private needService: NeedService) {}
+export class BasketComponent implements OnInit{
+  needs: Need[] = []
+  constructor(private needService: NeedService, private userService: UserService) {}
+  ngOnInit(): void {
+    this.userService.getBasket()
+  }
 
   checkout(): void {
 
   }
 
-  remove(): void {
+  remove(need: Need): void {
 
   }
 }
